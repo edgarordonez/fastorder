c0c33a1e376e5cc042bddce46bc0ef7de20eec7b
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-04-22 01:56:40 +0200

    feat(fastorder): add waiter dependencies

--- app/main/app/fastorder/fastorder/api/EntryPointDependencyContainer.scala
+++ app/main/app/fastorder/fastorder/api/EntryPointDependencyContainer.scala
@@ -1,10 +1,12 @@
 package app.fastorder.fastorder.api
 
 import app.fastorder.fastorder.api.controller.status.StatusGetController
-import app.fastorder.fastorder.api.controller.waiter.WaiterGetController
+import app.fastorder.fastorder.api.controller.waiter.{WaiterGetController, WaiterPostController}
+import app.fastorder.fastorder.waiter.infrastructure.dependency_injection.WaiterModuleDependencyContainer
 
-final class EntryPointDependencyContainer {
+final class EntryPointDependencyContainer(waiterDependencies: WaiterModuleDependencyContainer) {
   val statusGetController = new StatusGetController
 
-  val waiterGetController = new WaiterGetController
+  val waiterGetController = new WaiterGetController(waiterDependencies.waiterSearcher)
+  val waiterPostController = new WaiterPostController(waiterDependencies.waiterRegister)
 }
