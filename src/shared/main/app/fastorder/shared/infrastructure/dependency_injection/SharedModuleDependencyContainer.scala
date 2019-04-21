e4e3cbe1aacb45b50c59cf5744d22a26a8f1f337
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-04-22 01:50:45 +0200

    feat(shared): added doobieDbConnection

--- src/shared/main/app/fastorder/shared/infrastructure/dependency_injection/SharedModuleDependencyContainer.scala
+++ src/shared/main/app/fastorder/shared/infrastructure/dependency_injection/SharedModuleDependencyContainer.scala
@@ -2,10 +2,14 @@ package app.fastorder.shared.infrastructure.dependency_injection
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import app.fastorder.shared.infrastructure.doobie.{DoobieDbConnection, JdbcConfig}
+
 import scala.concurrent.ExecutionContextExecutor
 
-final class SharedModuleDependencyContainer(actorSystemName: String) {
+final class SharedModuleDependencyContainer(actorSystemName: String, jdbConfig: JdbcConfig) {
   implicit val system: ActorSystem               = ActorSystem(actorSystemName)
   val materializer: ActorMaterializer            = ActorMaterializer()
   val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val doobieDbConnection: DoobieDbConnection = new DoobieDbConnection(jdbConfig)
 }
