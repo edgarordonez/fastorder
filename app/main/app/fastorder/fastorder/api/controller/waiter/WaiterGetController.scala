4405afb785938d0cb783e9cc7f20a71888540b0d
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-04-22 01:54:52 +0200

    feat(fastorder): add post a controller and update get controller

--- app/main/app/fastorder/fastorder/api/controller/waiter/WaiterGetController.scala
+++ app/main/app/fastorder/fastorder/api/controller/waiter/WaiterGetController.scala
@@ -1,9 +1,12 @@
 package app.fastorder.fastorder.api.controller.waiter
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.StandardRoute
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+import app.fastorder.fastorder.waiter.infrastructure.marshaller.WaiterJsonFormatMarshaller._
+import app.fastorder.fastorder.waiter.application.search.WaiterSearcher
 
-final class WaiterGetController {
-  def get(): StandardRoute = complete(HttpEntity(ContentTypes.`application/json`, """{"status":"ok"}"""))
+final class WaiterGetController(searcher: WaiterSearcher) extends SprayJsonSupport with DefaultJsonProtocol {
+  def get(): StandardRoute = complete(searcher.all())
 }
---
+++ app/main/app/fastorder/fastorder/api/controller/waiter/WaiterPostController.scala
@@ -0,0 +1,17 @@
+package app.fastorder.fastorder.api.controller.waiter
+
+import akka.http.scaladsl.model.HttpResponse
+import akka.http.scaladsl.model.StatusCodes.Created
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.StandardRoute
+import app.fastorder.fastorder.shared.domain.waiter.WaiterId
+import app.fastorder.fastorder.waiter.application.register.WaiterRegister
+import app.fastorder.fastorder.waiter.domain.WaiterName
+
+final class WaiterPostController(register: WaiterRegister) {
+  def post(id: String, name: String): StandardRoute = {
+    register.register(WaiterId(id), WaiterName(name))
+
+    complete(HttpResponse(Created))
+  }
+}
