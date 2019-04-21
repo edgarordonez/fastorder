dca8486d4074a58c8141edb7a97bc4f1143f4e10
Edgar Ordóñez <edgar.rodriguez@kiwi.com>
2019-04-22 01:58:01 +0200

    feat(fastorder): waiter post route

--- app/main/app/fastorder/fastorder/api/Routes.scala
+++ app/main/app/fastorder/fastorder/api/Routes.scala
@@ -2,6 +2,9 @@ package app.fastorder.fastorder.api
 
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import spray.json.JsValue
 
 final class Routes(container: EntryPointDependencyContainer) {
   private val status = get {
@@ -10,7 +13,20 @@ final class Routes(container: EntryPointDependencyContainer) {
 
   private val waiter = get {
     path("waiters")(container.waiterGetController.get())
-  }
+  } ~
+    post {
+      path("waiters") {
+        jsonBody { body =>
+          container.waiterPostController.post(
+            body("id").convertTo[String],
+            body("name").convertTo[String]
+          )
+        }
+      }
+    }
+
+  private def jsonBody(handler: Map[String, JsValue] => Route): Route =
+    entity(as[JsValue])(json => handler(json.asJsObject.fields))
 
   val all: Route = status ~ waiter
 }
